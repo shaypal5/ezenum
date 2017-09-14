10221f4fd291d17c59e6a6535d30870cf4550c45
Shay Palachy <shaypal5@gmail.com>
2017-09-14 14:15:11 +0300

    plumbus

---
+++ plumbus-0.0.0.1/plumbus/__init__.py
@@ -0,0 +1,13 @@
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
+from .core import (
+    StringEnum,
+    string_enum,
+)
+try:
+    del core
+except NameError:
+    pass
